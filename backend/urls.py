921195670761aec01bdacf87fb73a2778d410912
welly091 <whtp1490@gmail.com>
2022-09-19 16:35:05 -0700

    reorganize git files

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("question/<int:pk>/", QuestionDetail.as_view(), name="post_detail"),
     path("comment", CommentList.as_view()),
     #Change this to foreign key to get all comments with specific question
-    path("comment/<int:pk>", CommentDetail.as_view()),
+    path("comment/<int:question.id>", CommentDetail.as_view()),
 
     path("create/", UserCreate.as_view(), name="user_create"),
 
